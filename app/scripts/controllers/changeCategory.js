93cbfbbe59dcf3dae2a98a52894deb8a51bea05b
XueQian <xueqian0321@126.com>
2014-09-01 19:25:21 +0800

    update html

--- app/scripts/controllers/changeCategory.js
+++ app/scripts/controllers/changeCategory.js
@@ -3,4 +3,7 @@
 angular.module('angularLeteusgoApp')
   .controller('changeCategoryCtrl', function ($scope, goodsItemService) {
 
+    var itemList = goodsItemService.loadItem();
+    $scope.products = goodsItemService.get('itemList');
+
   });
--- app/scripts/controllers/changeGoodsItems.js
+++ app/scripts/controllers/changeGoodsItems.js
@@ -2,5 +2,7 @@
 
 angular.module('angularLeteusgoApp')
   .controller('changeGoodsItemsCtrl', function ($scope, goodsItemService) {
+    var itemList = goodsItemService.loadItem();
+    $scope.products = goodsItemService.get('itemList');
 
   });
